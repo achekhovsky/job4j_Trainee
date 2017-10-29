714766ebd80be03ed343a6ce206528c6a1053517
achekhovsky <ringrabbit@rambler.ru>
2017-10-29 11:32:39 +0300

    1.1. Создание Hello World[#141]

---
+++ src/ru/job4j/Calculate.java
@@ -0,0 +1,15 @@
+package ru.job4j;
+
+/**
+ * My first class in job4j courses.
+ * @version 1.0
+ * @author achekhovsky
+ *
+ */
+public class Calculate 
+{
+	public static void main(String[] args) 
+	{
+		System.out.print("Hello world!");
+	}
+}
