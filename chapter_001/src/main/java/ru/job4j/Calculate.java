54c424a5efa198d297b90679c177f8609644ca4d
achekhovsky <ringrabbit@rambler.ru>
2017-10-29 13:39:36 +0300

    1.3. JavaDoc. Code convention[#4087]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -4,10 +4,13 @@ package ru.job4j;
  * My first class in job4j courses.
  * @version 1.0
  * @author achekhovsky
- *
  */
 public class Calculate 
-{
+{	
+	/**
+ 	* The entry point of the interpreter
+ 	*@param <strong>args</string> - the arguments on the command line
+ 	*/
 	public static void main(String[] args) 
 	{
 		System.out.print("Hello world!");
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,9 @@
+/**
+* Package for calculate task.
+*
+* @author achekhovsky
+* @version $Id$
+* @since 1.0
+*/
+
+package ru.job4j;
